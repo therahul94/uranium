c41089055e4b1936cfc4288d04542856d6f5d2ca
therahul94 <therahulkurmalkar@gmail.com>
2022-04-07 21:31:21 +0530

    assignment 07Apr

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,18 +3,58 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
+//problem 1)
+let movieList = ["Gangs of wasseypur", "Hachi: A dog's tale", "shawshank redemption", "When I first met", "Dhol"]
+router.get('/movies',function(req, res){
+    res.send(movieList)
 })
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+// problem 2) & 3)
+
+router.get('/movies/:indexNumber', function(req, res){
+    let i = req.params.indexNumber
+    if((i < 5) && (i >= 0)){
+        res.send(movieList[i])
+    }
+    else{
+        res.send("ERROR : \"Invalid index\"")
+    }
+})
+
+let moviesArr = [ {
+    id: 1,
+    name: 'The Shining'
+   }, {
+    id: 2,
+    name: 'Incendies'
+   }, {
+    id: 3,
+    name: 'Rang de Basanti'
+   }, {
+    id: 4,
+    name: 'Finding Nemo'
+   }]
+   
+// problem 4)
+router.get('/films', function(req, res){
+    res.send(moviesArr)
+})
+
+// problem 5)
+
+router.get('/films/:filmId', function (req, res) {
+    let getId = req.params.filmId
+
+    if ((getId > 0) && (getId < 5)){
+        for (let i=0; i<moviesArr.length; i++){
+            if(moviesArr[i].id == getId){
+                res.send(moviesArr[i])
+            }
+        }
+    }
+    else{
+        res.send('No movie exists with this id')
+    }
 });
 
 
