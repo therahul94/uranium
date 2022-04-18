d5cce262e73c05479538efbc3011d2a7920d6c9b
sabiha-thorium <sabiha.khan@functionup.org>
2022-04-18 13:19:29 +0530

    Assignment solution

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId
+let ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author:{
         type: ObjectId,
-        ref: "Author"
-    },
+        ref:"myNewAuthor"
+    } ,
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher: {
+        type: ObjectId,
+        ref: "Publisher"
+    },
+    isHardCover: {
+        type: Boolean,
+        default: false
+    }
 
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('myNewBook', bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
+const publisherController = require('../controllers/publisherController')
+const bookController= require("../controllers/bookController");
+const { route } = require('express/lib/application');
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -10,12 +12,12 @@ router.get("/test-me", function (req, res) {
 
 router.post("/createAuthor", authorController.createAuthor  )
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+router.post('/createPublisher', publisherController.createPublisher)
 
 router.post("/createBook", bookController.createBook  )
 
-router.get("/getBooksData", bookController.getBooksData)
+router.get('/get-all-books', bookController.fetchbooks)
 
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+router.put('/books', bookController.updateBooks)
 
 module.exports = router;
